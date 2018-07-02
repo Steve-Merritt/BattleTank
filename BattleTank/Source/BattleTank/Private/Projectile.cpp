d4fd56b121b71ce758eb23cbb78da3aaadaf4f26
Steve <scm191@gmail.com>
2018-07-01 23:16:32 -0700

    Aiming dot color change, tank and aiming component exposed, minor cleanup.

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -28,7 +28,7 @@ void AProjectile::Tick(float DeltaTime)
 
 void AProjectile::Launch(float Speed)
 {
-    if (ProjectileMovement)
+    if (ProjectileMovement != nullptr)
     {
         ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
         ProjectileMovement->Activate();
